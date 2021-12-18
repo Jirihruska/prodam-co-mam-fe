c6a170d1bd251e02363da744f34a40640389036d
jirihruska <georgepear1@seznam.cz>
2021-12-18 23:31:54 +0100

    Little changes

--- src/components/loginPage/LoginPage.vue
+++ src/components/loginPage/LoginPage.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="grid grid-cols-2 h-screen">
     <div class="flex h-screen bg-red-800 text-center">
-      <div class="m-auto text-4xl text-white font-semibold">
+      <div class="m-auto text-4xl text-white font-semibold select-none">
         Přihlášení
       </div>
     </div>
     <div class="flex h-screen">
-      <form class="m-auto">
+      <form class="m-auto w-1/2">
         <Input label="Email" />
         <Input label="Heslo" />
         <div class="flex justify-center pt-6">
-          <button class="bg-red-800 text-white p-4">
+          <button class="bg-red-800 text-white p-4 w-full hover:bg-red-900 text-md">
             Přihlásit
           </button>
         </div>
--- src/components/reusableComponents/Input.vue
+++ src/components/reusableComponents/Input.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="container pb-8">
-    <div class="text-right w-1/3">
-      <label class="text-red-800 text-xl font-semibold select-none">{{ label }}</label>
+    <div class="text-right w-1/5">
+      <label class="text-red-800 text-xl font-semibold select-none">{{
+        label
+      }}</label>
     </div>
     <div class="centered text-center">
       <input
-        class="
-          centered
-          focus:outline-none
-          bg-red-800
-          text-white text-lg
-          p-4
-        "
+        class="centered focus:outline-none bg-red-800 text-white text-lg p-4 w-full"
         :type="type"
         required
       />
@@ -37,21 +33,5 @@ export default {
 }
 .centered {
   justify-content: center;
-}
-
-::placeholder {
-  /* Chrome, Firefox, Opera, Safari 10.1+ */
-  color: rgb(255, 161, 161);
-  opacity: 1; /* Firefox */
-}
-
-:-ms-input-placeholder {
-  /* Internet Explorer 10-11 */
-  color: red;
-}
-
-::-ms-input-placeholder {
-  /* Microsoft Edge */
-  color: red;
 }
 </style>
