7d4df09749ff0aec29a3b61016c417e26283ae61
Thomas <klapetek.tom@gmail.com>
2021-12-18 18:35:44 +0100

    tvorba

--- src/components/registrationPage/RegistrationPage.vue
+++ src/components/registrationPage/RegistrationPage.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    Registration page
+  <div class='border-4 border-red-600 mt-2 mr-80 ml-10 pb-2 rounded-lg'>
+    <div class='text-3xl mt-5 ml-20 text-red-800 underline'>REGISTRACE NOVÉHO ÚČTU</div>
+    <div><input class='border-2 border-red-800 mt-8 ml-40' placeholder="Jméno" type="text"></div>
+    <div><input class='border-2 border-red-800 mt-2 ml-40' placeholder="Příjmení" type="text"></div>
+    <div><input class='border-2 border-red-800 mt-2 ml-40' placeholder="Email" type="email"></div>
+    <div><input class='border-2 border-red-800 mt-2 ml-40 pl-5' type="date"></div>
+    <div><input class='border-2 border-red-800 mt-2 ml-40' placeholder="Heslo" type="password"></div>
+    
   </div>
 </template>
 
