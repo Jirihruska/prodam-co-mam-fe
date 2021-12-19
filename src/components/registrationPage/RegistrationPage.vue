32b1f1290d893e210b3cc260357c70fb78931500
Thomas <klapetek.tom@gmail.com>
2021-12-19 12:52:26 +0100

    návrh reg

--- src/components/registrationPage/RegistrationPage.vue
+++ src/components/registrationPage/RegistrationPage.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-    registration
-  </div>
+  <div class="grid grid-cols-2 h-screen">
+    <div class="flex h-screen bg-red-800 text-center">
+      <div class="flex h-screen">
+      <form class="mt-40 ml-20">
+        <Input2 label="Jméno" />
+        <Input2 label="Příjmení" />
+        <Input2 label="Email" />
+        <Input2 label="Heslo" />
+        <div class="text-white">
+        </div>
+        <div class="flex justify-center pt-6">
+          <button class="bg-white font-bold text-red-800 p-4 ml-20 w-full hover:bg-gray-400 text-md">
+            Registrovat
+          </button>
+        </div>
+        <div class="text-white pt-8 ml-10 pl-10"> <input type="radio"> Souhlasím se zpracováním osobních údajů (GDPR)</div>
+      </form>
+    </div>
+    </div>
+    <div class="m-auto text-6xl text-red-800 font-semibold select-none">
+        Registrace
+      </div>
+    </div>  
 </template>
 
 <script>
+import Input2 from "../reusableComponents/Input2.vue";
 export default {
-
-}
+  components: {
+    Input2,
+  },
+};
 </script>
---
+++ src/components/reusableComponents/Input2.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container pb-8">
+    <div class="pl-20 w-1/5">
+      <label class="text-white text-xl font-semibold select-none">{{
+        label
+      }}</label>
+    </div>
+    <div class="centered text-center">
+      <input
+        class="centered focus:outline-none bg-white text-red-800 text-lg p-4 ml-20 w-full"
+        :type="type"
+        required
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: String,
+    placeholder: String,
+    label: String,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-self: center;
+}
+.centered {
+  justify-content: center;
+}
+</style>
